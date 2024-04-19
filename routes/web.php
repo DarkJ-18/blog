4d76110275ce550881593f816d8de64c1401d037
DarkJ-18 <j.juancamilojurado@gmail.com>
2024-04-19 00:05:56 -0500

    Se agrega lógica para crear un nuevo post en la ruta 'prueba'

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 
 Route::get('/', HomeController::class);
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
+
+Route::get('prueba', function () {
+   $post = new Post;
+   $post->title = 'Mi primer post 1';
+   $post->content = 'Contenido de mi primer post 1';
+   $post->category = 'Categoria de prueba 1';
+   $post->save();
+   return $post;
+});
