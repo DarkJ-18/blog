f26b7cbd4cd2db872a2efca2a44fe6b0920f0a52
DarkJ-18 <j.juancamilojurado@gmail.com>
2024-04-18 20:01:15 -0500

    Se corrige la estructura de la página principal en home.blade.php y se agrega el componente x-app-layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body>
+<x-app-layout>
     <div class="max-w-4xl mx-auto px-4">
         {{-- <h1>Bien venido a la pagina principal</h1> --}}
-        
+
         <x-alert type="dark" class="mb-4">
             <x-slot name="title">
                 Titulo de la alerta
             </x-slot>
             Contedio de la alerta
-        </x-alert2>
-        <p>hola mundo</p>
+            </x-alert2>
+            <p>hola mundo</p>
     </div>
-
-
-</body>
-
-</html>
+</x-app-layout>
