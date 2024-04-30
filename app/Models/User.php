d3f909606e890868d4b15aae476a02a87144c434
Juan. <54506019+DarkJ-18@users.noreply.github.com>
2024-04-30 17:24:22 +0000

    Se agrega relación uno a uno entre User y Address en el modelo User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function address()
+    {
+        return $this->hasOne(Address::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Models\Post;
+use App\Models\User;
 
 Route::get('/', HomeController::class);
 Route::get('/posts', [PostController::class, 'index']);
@@ -12,9 +13,9 @@ Route::get('/posts/{post}', [PostController::class, 'show']);
 
 Route::get('prueba', function () {
    // Crear un nuevo post
-   $post = Post::find(1);
-   
-   dd($post->is_active); // true
+   $user = User::find(1);
+   return $user->address; // Devuelve la dirección del usuario
+
 
 
 });
